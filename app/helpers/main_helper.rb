c09f0985a5c8673a2af1b04f061121560f865e30
Andreas Schröder <kaazoo@artcom.de>
2012-12-06 18:37:18 +0100

    add helper functions for temperature lookups

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,23 @@
 module MainHelper
+
+def get_cpu_temperture(hostname, user)
+  if user != nil
+    output = `ssh #{user}@#{hostname} sensors | grep "Core 0" | awk '{print $3}'`
+  else
+    output = `ssh #{hostname} sensors | grep "Core 0" | awk '{print $3}'`
+  end
+  puts output
+  return output
+end
+
+def get_board_temperture(hostname, user)
+  if user != nil
+    output = `ssh #{user}@#{hostname} /opt/dell/srvadmin/bin/omreport chassis temps index=0 | grep Reading | awk '{print $3}'`
+  else
+    output = `ssh #{hostname} /opt/dell/srvadmin/bin/omreport chassis temps index=0 | grep Reading | awk '{print $3}'`
+  end
+  puts output
+  return output
+end
+
 end
