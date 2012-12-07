2f2391f44930b36c784f7063a01cdadff727d719
Andreas Schröder <kaazoo@artcom.de>
2012-12-07 12:45:17 +0100

    support different measurement types filter out false results

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,23 +1,50 @@
 module MainHelper
 
-def get_cpu_temperture(hostname, user)
+def get_cpu_temperture(measure_type, hostname, user)
   if user != nil
     output = `ssh #{user}@#{hostname} sensors | grep "Core 0" | awk '{print $3}'`
   else
     output = `ssh #{hostname} sensors | grep "Core 0" | awk '{print $3}'`
   end
+
+  # replace +°C
+  output = output.to_f
+
   puts output
-  return output
+
+  # do not return false results
+  if (output == 0.0) || (output < 0) || (output > 120)
+    return nil
+  else
+    return output
+  end
 end
 
-def get_board_temperture(hostname, user)
-  if user != nil
-    output = `ssh #{user}@#{hostname} /opt/dell/srvadmin/bin/omreport chassis temps index=0 | grep Reading | awk '{print $3}'`
-  else
-    output = `ssh #{hostname} /opt/dell/srvadmin/bin/omreport chassis temps index=0 | grep Reading | awk '{print $3}'`
+def get_board_temperture(measure_type, hostname, user)
+  case measure_type
+    when "openmanage"
+      command = "/opt/dell/srvadmin/bin/omreport chassis temps index=0 | grep Reading | awk '{print $3}'"
+    else
+      command = "sensors | grep temp1 | awk '{print $2}'"
   end
+
+  if user != nil
+    output = `ssh #{user}@#{hostname} #{command}`
+  else
+    output = `ssh #{hostname} #{command}`
+  end
+
+  # replace +°C
+  output = output.to_f
+
   puts output
-  return output
+
+  # do not return false results
+  if (output == 0.0) || (output < 0) || (output > 120)
+    return nil
+  else
+    return output
+  end
 end
 
 end
