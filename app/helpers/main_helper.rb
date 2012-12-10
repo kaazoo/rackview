5cd5f319e14c8293b7afd7a3017b5c6203fcecf5
Andreas Schröder <kaazoo@artcom.de>
2012-12-10 17:20:52 +0100

    add support for sandbox and easyraid devices

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,11 +1,38 @@
 module MainHelper
 
+def call_ssh(command)
+  require 'timeout'
+
+  pipe = "foo"
+  output = "bar"
+
+  begin
+    Timeout.timeout(5) {
+      pipe = IO.popen(command)
+      pid = pipe.pid
+      Process.wait pipe.pid
+      output = pipe.read
+      puts "output of pipe.read(): " + output.to_s
+  }
+  rescue Timeout::Error
+    Process.kill 9, pipe.pid
+    Process.wait pipe.pid # we need to collect status so it doesn't stick around as zombie process
+    output = nil
+  end
+
+  return output
+
+end
+
+
 def get_cpu_temperture(measure_type, hostname, user)
   case measure_type
     when "coretemp"
       command = "sensors | grep \"Core 0\" | awk '{print $3}'"
     when "vt1211"
       command = "sensors | grep \"CPU Temp\" | awk '{print $3}'"
+    when "vt1211_sio"
+      command = "sensors | grep \"SIO Temp\" | awk '{print $3}'"
     when "tempmonitor"
       command = "/Applications/TemperatureMonitor.app/Contents/MacOS/tempmonitor -a -l -ds | grep \"SMC CPU A HEAT SINK\" | awk '{print $6}'"
     else
@@ -14,15 +41,17 @@ def get_cpu_temperture(measure_type, hostname, user)
   end
 
   if user != nil
-    output = `ssh #{user}@#{hostname} #{command}`
+    ssh_command = "ssh #{user}@#{hostname} #{command}"
   else
-    output = `ssh #{hostname} #{command}`
+    ssh_command = "ssh #{hostname} #{command}"
   end
+
+  output = call_ssh(ssh_command)
 
   # replace +°C
   output = output.to_f
 
-  puts output
+  puts "output of call_ssh(): " + output.to_s
 
   # do not return false results
   if (output == 0.0) || (output < 0) || (output > 120)
@@ -31,6 +60,7 @@ def get_cpu_temperture(measure_type, hostname, user)
     return output
   end
 end
+
 
 def get_board_temperture(measure_type, hostname, user)
   case measure_type
@@ -50,15 +80,17 @@ def get_board_temperture(measure_type, hostname, user)
   end
 
   if user != nil
-    output = `ssh #{user}@#{hostname} #{command}`
+    ssh_command = "ssh #{user}@#{hostname} #{command}"
   else
-    output = `ssh #{hostname} #{command}`
+    ssh_command = "ssh #{hostname} #{command}"
   end
+
+  output = call_ssh(ssh_command)
 
   # replace +°C
   output = output.to_f
 
-  puts output
+  puts "output of call_ssh(): " + output.to_s
 
   # do not return false results
   if (output == 0.0) || (output < 0) || (output > 120)
@@ -67,5 +99,98 @@ def get_board_temperture(measure_type, hostname, user)
     return output
   end
 end
+
+
+def get_sanbox_temperture(hostname, user)
+  require 'greenletters'
+
+  log = ""
+  telnet = Greenletters::Process.new("telnet -l #{user} #{hostname}", :transcript => log)
+
+  # Install a handler which may be triggered at any time
+  telnet.on(:output, /Password/i) do |process, match_data|
+    telnet << File.read(File.expand_path("../../../config/sanbox_pw", __FILE__))
+  end
+
+  puts "Starting Telnet ..."
+  telnet.start!
+
+  # wait for prompt
+  telnet.wait_for(:output, /#{hostname} #>/i)
+
+  # show temperature
+  telnet << "show chassis\n"
+  telnet.wait_for(:output, /#{hostname} #>/i)
+
+  # quit session
+  telnet << "exit\n"
+  telnet.wait_for(:output, /Good bye./i)
+  puts "Telnet has exited."
+
+  lines = log.split("\n")
+
+  lines.each do |line|
+    if line.include?("BoardTemp")
+	  temp = line.split(" ")
+	  return temp[6].to_f
+    end
+  end
+end
+
+
+def get_easyraid_temperture(hostname, user, firmware_version)
+  require 'greenletters'
+
+  log = ""
+  telnet = Greenletters::Process.new("telnet #{hostname}", :transcript => log)
+
+  # Install a handler which may be triggered at any time
+  telnet.on(:output, /Please input user name:/i) do |process, match_data|
+    telnet << "#{user}\n"
+    telnet << File.read(File.expand_path("../../../config/easyraid_pw", __FILE__))
+  end
+
+  puts "Starting Telnet ..."
+  telnet.start!
+
+  case firmware_version
+    when 1.43
+      prompt = "ACSCLI"
+      enclosure = "enc0"
+      sensor = "BP_TEMP1"
+
+    when 1.09
+      prompt = "CLI"
+      enclosure = "enca"
+      sensor = "[BP]TEMP1"
+
+    else
+      puts "unknown version :" + version.to_s
+      return nil
+  end
+
+  # wait for prompt
+  telnet.wait_for(:output, /#{prompt}>/i)
+
+  # show temperature
+  telnet << "enclist #{enclosure} all\n"
+  telnet.wait_for(:output, /#{prompt}>/i)
+
+  # quit session
+  telnet << "quit\n"
+  telnet.wait_for(:output, /Done/i)
+  puts "Telnet has exited."
+
+  lines = log.split("\n")
+
+  lines.each do |line|
+    if line.include?(sensor)
+      puts line
+	  temp = line.split(" ")[1].split("/")
+	  return temp[0].to_f
+    end
+  end
+end
+
 
 end
